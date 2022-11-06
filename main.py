b01a1bf4c33e6601bc7bad0f6cdfcce690aae8e9
kgoering <karl_reinhold.goering@smail.th-koeln.de>
2022-11-06 18:35:40 +0100

    prep

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-
 def main():
-
     print('Hi')
-    #test
+    # test
+
+
+def get(image):
+    #todo
+    return
+
+def getMostSimilar(image):
+    #todo
+    return
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-
