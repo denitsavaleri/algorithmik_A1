3cb6fc29722e0c3fef18ef24888c5f401e9c4002
kgoering <karl_reinhold.goering@smail.th-koeln.de>
2022-11-09 18:23:39 +0100

    Bilder aufgeräumt, Imagepathliste drin

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-def main():
-    print('Hi')
-    # test
+import os
+from pathlib import Path
+from typing import List
 
+def getImagePaths():
+    projectDir = os.path.dirname(os.path.realpath('__file__'))
+
+    moduleName = "Images"
+
+    pathToFile= os.path.join(projectDir, moduleName)
+    listOfAllImagePaths : List = []
+
+    for path in Path(pathToFile).rglob('*.jpg'):
+     listOfAllImagePaths.append(os.path.join(pathToFile, path))
+
+    return listOfAllImagePaths
 
 def get(image):
     #todo
     return
 
+
 def getMostSimilar(image):
     #todo
     return
 
+def main():
+    print(getImagePaths().__len__())
+    # test
+
 
 # Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     main()
